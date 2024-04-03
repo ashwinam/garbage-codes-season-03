ae45c3ad9e1fdde0ddc4555e15dd9e2377f1fb72
ashwinPy <ashwinam31@gmail.com>
2024-04-03 13:09:59 +0530

    add basics file

--- pythons revision/basics.py
+++ pythons revision/basics.py
@@ -18,8 +18,25 @@ print(greeting)
 1. Integer
 2. Floating-point numbers
 3. String
+4. Boolean (True and False)
 '''
 
 print(type('ashwin'))
 print(type(10))
 print(type(10.02))
+
+print('cat', 'mouse', 'lion', sep='-')
+# print('cat', 'mouse', 'lion', end='')
+
+# usr_input = input('Type whatever you want: \n')
+# print(usr_input)
+
+print(len('ashwin'))
+
+print(str(20))
+print(int(20))
+print(float(20))
+print(list([20, 10]))
+print(tuple([20]))
+print(bool(20))
+print(set([20, 20]))
