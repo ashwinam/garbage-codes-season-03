49f39c10609d8581313e11cae204aec288212a0d
ashwinPy <ashwinam31@gmail.com>
2024-04-03 12:55:48 +0530

    python data types

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ print(6 % 2)
 greeting = 'Hello'
 greeting += ' ' + 'World'
 print(greeting)
+
+# Data types (That holds single value or classification of the value but for the developer)
+
+'''
+1. Integer
+2. Floating-point numbers
+3. String
+'''
+
+print(type('ashwin'))
+print(type(10))
+print(type(10.02))
